49b9a3ff655e26658ae8c1773d8cdd53f77d3400
sindhujamallappa <72853544+sindhujamallappa@users.noreply.github.com>
2021-10-23 09:01:04 +0530

    Update array.c

--- array.c
+++ array.c
@@ -17,3 +17,29 @@ int main()
         printf("%d\t", arr[j]);   
     }
 }
+
+//Reverse the user input array without writing a seperate  function
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,j,m,n;
+    int *arr;
+    printf("Enter the array size");
+    scanf("%d", &n);
+    arr=(int*)malloc(n*sizeof(int));
+    for(i=1;i<=n;i++)
+    {
+        printf("Enter the Element %d", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("Actual array is:\n");
+    for(j=1;j<=n;j++){
+        printf("%d\t", arr[j]);   
+    }
+    printf("\n Reversed array is \n");
+    for(j=n;j>=1;j--){
+        printf("%d\t", arr[j]);   
+    }
+    
+}
