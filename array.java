00b9dd16e297d3e485e2fb5f070a761fcb7fe081
sindhujamallappa <72853544+sindhujamallappa@users.noreply.github.com>
2021-10-24 08:38:14 +0530

    Update array.java

--- array.java
+++ array.java
@@ -5,11 +5,11 @@ public class Main
     public static void main(String[] args)
     {
         int n;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in); //Declaring and creating a scanner object, use the sc variable in any method in the class. To create a Scanner object, you use the new keyword followed by a call to the Scanner class constructor.
         System.out.println("Enter the number of Elements you want to store: ");
     
-        n=sc.nextInt();
-        int[]array=new int[10];
+        n=sc.nextInt(); //used to scan the next token of the input as an int.
+        int[]array=new int[10]; //int[] is an array of primitive int values
         System.out.println("Enter the Elements: ");
         for(int i=0;i<n;i++)
         {
